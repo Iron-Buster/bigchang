f237fe6d51b76d582d414aca288d3eb541375cc2
ikun <1457480465@qq.com>
2023-03-02 21:25:31 +0800

    设计题

---
+++ LeetCode/Design/LC_2502.java
@@ -0,0 +1,28 @@
+package LeetCode.Design;
+
+public class LC_2502 {
+
+
+//    2502. 设计内存分配器
+    class Allocator {
+
+        public Allocator(int n) {
+
+        }
+
+        public int allocate(int size, int mID) {
+
+
+            return -1;
+        }
+
+        public int free(int mID) {
+
+            return -1;
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
