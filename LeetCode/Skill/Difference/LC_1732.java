6463733738599732b8fe39fd8408da0d2d52361e
ikun <1457480465@qq.com>
2023-03-03 17:17:00 +0800

    差分数组

---
+++ LeetCode/Skill/Difference/LC_1732.java
@@ -0,0 +1,22 @@
+package LeetCode.Skill.Difference;
+
+public class LC_1732 {
+
+
+//    1732. 找到最高海拔
+    
+    public int largestAltitude(int[] gain) {
+        // 差分数组的前缀和 是原数组
+        int s = 0, ans = 0;
+        for (int x : gain) {
+            s += x;
+            ans = Math.max(ans, s);
+        }
+        return ans;
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
